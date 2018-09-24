51ffe686aa36d30f8a32d8bdfc59b35e0761b31e
michelleliza <32363109+michelleliza@users.noreply.github.com>
2018-09-24 15:59:53 +0700

    Add hillclimbing algorithm

---
+++ hillclimb.py
@@ -0,0 +1,29 @@
+import board as b
+import copy
+
+class HillClimbing():
+    def __init__(self, listPawn):
+        #initial config
+        self.board = b.Board(listPawn)
+        stop = False
+
+        while not stop:
+            currentCost = self.board.cost()
+            tempBoard = copy.deepcopy(self.board)
+            minCost = currentCost
+            for pawn in tempBoard.listPawn:
+                for i in range(0,8):
+                    for j in range(0,8):
+                        found = False
+                        for pawn2 in tempBoard.listPawn:
+                            if i == pawn2.x and j == pawn2.y:
+                                found = True
+                        if not found:
+                            pawn.x = i
+                            pawn.y = j
+                            newCost = tempBoard.cost()
+                            if newCost < minCost:
+                                minCost = newCost
+                                self.board = copy.deepcopy(tempBoard)
+            if minCost == currentCost: #stuck
+                stop = True #stop searching 
