e208d17fe46df35d47ef015ccd92a75cba46dfe8
michelleliza <32363109+michelleliza@users.noreply.github.com>
2018-09-27 16:07:52 +0700

    Added new heuristics + comment

--- hillclimb.py
+++ hillclimb.py
@@ -7,24 +7,33 @@ class HillClimbing():
         self.board = b.Board(listPawn)
         self.board.initPawn()
         stop = False
-
         while not stop:
             currentCost = self.board.cost()
-            tempBoard = copy.deepcopy(self.board)
+            tempBoard = copy.deepcopy(self.board) #new board to move pawns
             minCost = currentCost
             for pawn in tempBoard.listPawn:
+                maxHit = 0
+                minHit = 100
                 for i in range(0,8):
                     for j in range(0,8):
                         found = False
                         for pawn2 in tempBoard.listPawn:
                             if i == pawn2.x and j == pawn2.y:
                                 found = True
-                        if not found:
+                        if not found: #no conflicting pawns
                             pawn.x = i
                             pawn.y = j
                             newCost = tempBoard.cost()
-                            if newCost < minCost:
+                            if newCost < minCost: #climbing
                                 minCost = newCost
                                 self.board = copy.deepcopy(tempBoard)
+                            elif newCost == minCost:
+                                crtHitSame, crtHitDiff = pawn.hit(tempBoard.listPawn)
+                                if crtHitSame > maxHit: #move pawn with more hits to same colored pawns
+                                    maxHit = crtHitSame
+                                    self.board = copy.deepcopy(tempBoard)
+                                if crtHitDiff < minHit: #move pawn with less hits to different colored pawns
+                                    minHit = crtHitDiff
+                                    self.board = copy.deepcopy(tempBoard)
             if minCost == currentCost: #stuck
-                stop = True #stop searching 
+                stop = True #stop searching
